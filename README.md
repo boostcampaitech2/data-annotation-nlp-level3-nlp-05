8df6397af4f185315de4bc839c0dbd0915fd2120
zgotter <shkim4738@gmail.com>
2021-11-18 04:33:32 +0000

    fix: make_relation_class.py 기능 make_data로 통합

--- README.md
+++ README.md
@@ -5,10 +5,11 @@
 - 데이터 생성
 
 ```bash
-python make_data.py --data_type [annotation/iaa/train]
+python make_data.py --data_type train
 ```
 
 - `data_type` : 생성할 데이터의 종류
+  - `relation`: relation class dictionary pickle 생성
   - `annotation`: relation annotating을 위한 데이터 생성
   - `iaa`: iaa 계산을 위한 데이터 생성
   - `train`: klue-re task 모델 학습을 위한 데이터 생성
--- make_data.py
+++ make_data.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import re
+import pickle
 import pandas as pd
 from utils import get_tagtog_df, get_label_to_num, get_num_to_label
 
@@ -10,6 +11,18 @@ def get_eng_name(data):
     result = re.findall(r"\([a-z]+:[a-z_]+\)|\([a-z_]+\)", data)[0]
     result = result.replace('(','').replace(')','')
     return result
+
+def make_relation_class_dict():
+    relation_class_df = pd.read_csv(os.path.join(file_path, 'relation_class.csv'))
+    
+    dict_label_to_num = {en: id for en, id in zip(relation_class_df['eng_name'], relation_class_df['id'])}
+    dict_num_to_label = {v:k for k, v in dict_label_to_num.items()}
+    
+    with open(os.path.join(file_path, 'dict_label_to_num.pkl'), 'wb') as f:
+        pickle.dump(dict_label_to_num, f)
+
+    with open(os.path.join(file_path, 'dict_num_to_label.pkl'), 'wb') as f:
+        pickle.dump(dict_num_to_label, f) 
 
 def make_iaa_data():
     raw_df = pd.read_excel(os.path.join(file_path, 'iaa_tourist_spot_raw.xlsx'), engine='openpyxl')
@@ -34,13 +47,15 @@ def make_train_data():
 
 def get_args():
     parser = argparse.ArgumentParser(description="make data arguments")
-    parser.add_argument("--data_type", required=True, help="generation data type", choices=['annotation', 'iaa', 'train'])
+    parser.add_argument("--data_type", required=True, help="generation data type", choices=['relation', 'annotation', 'iaa', 'train'])
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = get_args()
-    if args.data_type == 'annotation':
+    if args.data_type == 'relation':
+        make_relation_class_dict()
+    elif args.data_type == 'annotation':
         make_annot_data()
     elif args.data_type == 'iaa':
         make_iaa_data()
